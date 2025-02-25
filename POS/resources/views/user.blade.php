56aa4fbbc8eb3e83fb9811807247859899f81be9
TNCP06 <ctionusa6@gmail.com>
2025-02-26 02:03:07 +0700

    Add: Tugas Praktikum Jobsheet 02 - PWL

---
+++ POS/resources/views/user.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POS User</title>
+</head>
+<body>
+    <h1>POS User</h1>
+    <p>ID: {{ $id }}</p>
+    <p>Name: {{ $name }}</p>
+</body>
+</html>
