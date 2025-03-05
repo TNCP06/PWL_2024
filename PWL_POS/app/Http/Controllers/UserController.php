512c437237f55eea3d5c5969d1960a39199cdac2
TNCP06 <ctionusa6@gmail.com>
2025-03-05 09:42:02 +0700

    Praktikum 2.3

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -22,13 +22,42 @@ class UserController extends Controller
         // ];
         // UserModel::insert($data); // tambahkan data ke tabel m_user
 
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data);
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data);
+
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+
+        // Ambil model dengan kunci utamanya...
+        // $user = UserModel::find(1);
+
+        // Ambil model pertama yang cocok dengan batasan kueri...
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::where('username', 'manager')->firstOrFail();
+
+        // Alternatif untuk mengambil model pertama yang cocok dengan batasan kueri...
+        // $user = UserModel::firstWhere('level_id', 1);
+
+        // Mencari primary_key 1, menampilkan kolom username & nama, abort jika tidak ada
+        // $user = UserModel::findOr(20, ['username', 'nama'], function() {
+        //     abort(404);
+        // });
+
+        // $user = UserModel::findOrFail(1);
+
+        // Menghitung jumlah dengan kolom level_id bernilai 2
+        $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
 
         // coba akses model userModel
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::all();
+        return view('user', ['d' => $user]);
     }
 }
--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -7,19 +7,21 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>ID</th>
+            {{-- <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <th>ID Level Pengguna</th> --}}
+            <th>Jumlah Pengguna</th>
         </tr>
-        @foreach ($data as $d)
+        {{-- @foreach ($data as $d) --}}
             <tr>
-                <td>{{ $d->user_id }}</td>
+                {{-- <td>{{ $d->user_id }}</td>
                 <td>{{ $d->username }}</td>
                 <td>{{ $d->nama }}</td>
-                <td>{{ $d->level_id }}</td>
+                <td>{{ $d->level_id }}</td> --}}
+                <td>{{ $d }}</td>
             </tr>
-        @endforeach
+        {{-- @endforeach --}}
     </table>
 </body>
 </html>
