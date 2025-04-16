04bff55cb00d63b4df4774cd78aff800ceec63f8
TNCP06 <ctionusa6@gmail.com>
2025-04-16 08:38:34 +0700

    Jobsheet 7 - Tugas 1

--- PWL_POS/app/Models/UserModel.php
+++ PWL_POS/app/Models/UserModel.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
@@ -18,6 +19,9 @@ class UserModel extends Model
 
     // protected $guarded = ['created_at'];
     // akan mengabaikan kolom yang dimasukkan
+
+    protected $hidden = ['password']; // jangan ditampilkan saat select
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis dihash
 
     public function level(): BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
